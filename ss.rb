172ffe94fe08199aec7d4c6fe5daa00b1d78cdec
carusocr <carusocr@ldc.upenn.edu>
2015-01-20 16:12:54 -0500

    added haml

--- ss.rb
+++ ss.rb
@@ -1,2 +1,18 @@
+=begin
+Shopper home page should list...what?
+
+1. list of available stores, checkboxes by each.
+2. text box to accept search term. Each entry will populate the searchterm array.
+3. 'Shop!' button.
+
+=end
+
 require 'sinatra'
+require 'haml'
 require 'capybara'
+
+get '/' do
+
+  haml :home
+
+end
